dba0d439a7ebd194d4668b176ec2d630f5518f14
Jeremy <jeremias@availproject.org>
2025-03-25 08:23:19 -0300

    feat: Adds better handling for outdated VectorX contract state

--- src/main.rs
+++ src/main.rs
@@ -461,12 +461,24 @@ async fn get_eth_proof(
                 error: Some(data),
                 ..
             }) => {
-                tracing::error!("❌ Succinct API returned unsuccessfully");
-                return (
-                    StatusCode::NOT_FOUND,
-                    [("Cache-Control", "max-age=60, must-revalidate".to_string())],
-                    Json(json!({ "error": data })),
-                );
+                match data.to_string().contains("is not in the range of blocks") {
+                    true => {
+                        tracing::warn!("Succinct VectorX contract not updated yet, try again in a few minutes");
+                        return (
+                            StatusCode::TOO_EARLY,
+                            [("Cache-Control", format!("max-age={}, must-revalidate", eth_proof_cache_maxage))],
+                            Json(json!({ "error": data })),
+                        );
+                    }
+                    _ => {
+                        tracing::error!("❌ Succinct API returned unsuccessfully");
+                        return (
+                            StatusCode::NOT_FOUND,
+                            [("Cache-Control", "max-age=60, must-revalidate".to_string())],
+                            Json(json!({ "error": data }))
+                        )
+                    }
+                }
             }
             Err(err) => {
                 tracing::error!("❌ {:?}", err);
