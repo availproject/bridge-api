efd3955b35297a5d3bff2909e598440f283b7b54
Saša Pršić <s.prsic@gmail.com>
2026-02-20 12:05:02 +0100

    Api redesign (#50)
    * Remove non v1 api endpoints.
    
    * Remove succinct api and move models.
    
    * Update deps.
    
    * Move skipped models.
    
    * Add eth fetch tx.
    
    * Add endpoint to insert tx.
    
    * Fetch tx receipt and get message id.
    
    * Decode tx events.
    
    * Fix inputs.
    
    * Transaction response WIP.
    
    * Transactions db query wip.
    
    * Models wip for eth.
    
    * Add time estimate.
    
    * wip query
    
    * Add avail send query.
    
    * Add time estimate on avail transaction claim.
    
    * Tidy query.
    
    * Tidy.
    
    * Tidy query.
    
    * Skip null fields.
    
    * Fix tx insert.
    
    * Add doc.
    
    * Rename status.
    
    * Tidy query.
    
    * Add .sqlx
    
    * Tidy tx data.
    
    * Extract insert query.
    
    * fmt.
    
    * Update deps.
    
    * Update query.
    
    * Tidy query.
    
    * Make signature address optional.
    
    * Match only successful ext on avail side.
    
    * Change type to BigDecimal.
    
    * fix ext success.
    
    * fix ext success or null.
    
    * add missing sqlx file.
    
    * Fix avail query.
    
    * Update db mapping.
    
    * Update query.
    
    * Move fn.
    
    * Add error rsp.
    
    * fix: txn query api + remove insert txn endpoint
    
    * update insert query.
    
    * Update readme.
    
    * Update error responses.
    
    * Update doc.
    
    * add timestamp
    
    * feat: Fix logging + base img
    
    * Fix claim estimate.
    
    * Add debug log.
    
    * Remove log.
    
    * Fix fetching block number.
    
    * Add 10s cache.
    
    * fmt.
    
    ---------
    
    Co-authored-by: Abheek Tripathy <abheek.tripathy@gmail.com>
    Co-authored-by: Jeremy <jeremias@availproject.org>

---
+++ sql/query_eth_tx.sql
@@ -0,0 +1,28 @@
+SELECT be.message_id,
+       be.sender,
+       be.receiver,
+       be.amount,
+       be.source_block_hash,
+       be.source_transaction_hash,
+       be.block_number as source_block_height,
+       be.timestamp,
+       ai.block_height as "destination_block_height?: i32",
+       ai.ext_index    as "destination_tx_index?: i32",
+       COALESCE(
+               CASE
+                   WHEN be.status = 'in_progress' AND aet.message_id IS NULL THEN 'in_progress'::status
+                   WHEN be.status = 'in_progress' AND aet.message_id IS NOT NULL THEN 'bridged'::status
+                   ELSE be.status
+                   END,
+               'in_progress'::status
+       ) ::status      AS "final_status!: BridgeStatusEnum"
+FROM bridge_event be
+         LEFT JOIN public.avail_execute_table AS aet
+                   ON be.message_id = aet.message_id
+         LEFT JOIN public.avail_indexer AS ai on ai.id = aet.id
+WHERE be.sender = $1
+    AND be.event_type = $2
+    AND (ai.ext_success = $3
+   OR ai.ext_success is null)
+ORDER BY be.timestamp DESC
+LIMIT 1000
--- src/models.rs
+++ src/models.rs
@@ -1,102 +1,404 @@
-use crate::schema::sql_types::Status;
-use chrono::NaiveDateTime;
-use diesel::pg::{Pg, PgValue};
-use diesel::serialize::{IsNull, Output};
-use diesel::{
-    deserialize::{self, FromSql},
-    expression::AsExpression,
-    serialize::ToSql,
-    *,
-};
+use alloy::primitives::{Address, B256};
+use alloy::sol;
+use avail_core::data_proof::AddressedMessage;
+use axum::Json;
+use axum::response::{IntoResponse, Response};
+use bigdecimal::BigDecimal;
+use http::{HeaderMap, HeaderName, HeaderValue, StatusCode};
 use jsonrpsee::core::Serialize;
-use serde::Deserialize;
+use serde::{Deserialize, Deserializer};
+use serde_json::json;
 use serde_with::serde_as;
-use std::io::Write;
+use sp_core::H160;
+use sqlx::FromRow;
 
-#[derive(Debug, Clone, PartialEq, FromSqlRow, AsExpression, Eq)]
-#[diesel(sql_type = Status)]
+sol!(
+    #[allow(missing_docs)]
+    #[sol(rpc)]
+    SP1Vector,
+    "src/abi/SP1Vector.json"
+);
+
+#[derive(Debug, Deserialize)]
+pub struct Root {
+    pub data: Data,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Data {
+    pub message: Message,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Message {
+    pub slot: String,
+    pub body: MessageBody,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct MessageBody {
+    pub execution_payload: ExecutionPayload,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ExecutionPayload {
+    pub block_number: String,
+    pub block_hash: String,
+}
+
+pub struct ErrorResponse {
+    pub error: anyhow::Error,
+    pub headers_keypairs: Vec<(String, String)>,
+    pub status_code: Option<StatusCode>,
+}
+
+impl ErrorResponse {
+    pub fn new(error: anyhow::Error) -> Self {
+        Self {
+            error,
+            headers_keypairs: vec![],
+            status_code: None,
+        }
+    }
+    pub fn with_status(error: anyhow::Error, status_code: StatusCode) -> Self {
+        Self {
+            error,
+            headers_keypairs: vec![],
+            status_code: Some(status_code),
+        }
+    }
+
+    pub fn with_status_and_headers(
+        error: anyhow::Error,
+        status_code: StatusCode,
+        headers: &[(&str, &str)],
+    ) -> Self {
+        let h = headers
+            .iter()
+            .map(|(k, v)| (k.to_string(), v.to_string()))
+            .collect::<Vec<_>>();
+        Self {
+            error,
+            headers_keypairs: h,
+            status_code: Some(status_code),
+        }
+    }
+}
+
+// Tell axum how to convert `AppError` into a response.
+impl IntoResponse for ErrorResponse {
+    fn into_response(self) -> Response {
+        let status = self
+            .status_code
+            .unwrap_or(StatusCode::INTERNAL_SERVER_ERROR);
+        let mut headermap = HeaderMap::new();
+        for (k, v) in self.headers_keypairs {
+            headermap.insert(
+                HeaderName::try_from(k).unwrap(),
+                HeaderValue::try_from(v).unwrap(),
+            );
+        }
+        let json_resp = Json(json!({"error" : format!("{:#}", self.error)}));
+
+        (status, headermap, json_resp).into_response()
+    }
+}
+
+// This enables using `?` on functions that return `Result<_, anyhow::Error>` to turn them into
+// `Result<_, AppError>`. That way you don't need to do that manually.
+impl<E> From<E> for ErrorResponse
+where
+    E: Into<anyhow::Error>,
+{
+    fn from(err: E) -> Self {
+        Self {
+            error: err.into(),
+            headers_keypairs: vec![],
+            status_code: None,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Chain {
+    pub rpc_url: String,
+    pub contract_address: Address,
+}
+
+#[derive(Deserialize)]
+pub struct IndexStruct {
+    pub index: u32,
+}
+
+#[derive(Deserialize)]
+pub struct ProofQueryStruct {
+    pub index: u32,
+    pub block_hash: B256,
+}
+
+#[derive(Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct KateQueryDataProofResponse {
+    pub data_proof: DataProof,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub message: Option<AddressedMessage>,
+}
+
+#[derive(Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct DataProof {
+    pub roots: Roots,
+    pub proof: Vec<B256>,
+    pub leaf_index: u32,
+    pub leaf: B256,
+}
+
+#[derive(Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Roots {
+    pub data_root: B256,
+    pub blob_root: B256,
+    pub bridge_root: B256,
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct AccountStorageProofResponse {
+    pub account_proof: Vec<String>,
+    pub storage_proof: Vec<StorageProof>,
+}
+
+#[derive(Deserialize)]
+pub struct StorageProof {
+    pub proof: Vec<String>,
+}
+
+#[derive(Deserialize)]
+pub struct MekrleProofAPIResponse {
+    pub data: Option<MerkleProofData>,
+    pub error: Option<String>,
+    pub success: Option<bool>,
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct MerkleProofData {
+    pub range_hash: B256,
+    pub data_commitment: B256,
+    pub merkle_branch: Vec<B256>,
+    pub index: u16,
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct AggregatedResponse {
+    pub data_root_proof: Vec<B256>,
+    pub leaf_proof: Vec<B256>,
+    pub range_hash: B256,
+    pub data_root_index: u16,
+    pub leaf: B256,
+    pub leaf_index: u32,
+    pub data_root: B256,
+    pub blob_root: B256,
+    pub bridge_root: B256,
+    pub data_root_commitment: B256,
+    pub block_hash: B256,
+    pub message: Option<AddressedMessage>,
+}
+
+impl AggregatedResponse {
+    pub fn new(
+        range_data: MerkleProofData,
+        data_proof_res: KateQueryDataProofResponse,
+        hash: B256,
+    ) -> Self {
+        AggregatedResponse {
+            data_root_proof: range_data.merkle_branch,
+            leaf_proof: data_proof_res.data_proof.proof,
+            range_hash: range_data.range_hash,
+            data_root_index: range_data.index,
+            leaf: data_proof_res.data_proof.leaf,
+            leaf_index: data_proof_res.data_proof.leaf_index,
+            data_root: data_proof_res.data_proof.roots.data_root,
+            blob_root: data_proof_res.data_proof.roots.blob_root,
+            bridge_root: data_proof_res.data_proof.roots.bridge_root,
+            data_root_commitment: range_data.data_commitment,
+            block_hash: hash,
+            message: data_proof_res.message,
+        }
+    }
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct EthProofResponse {
+    pub account_proof: Vec<String>,
+    pub storage_proof: Vec<String>,
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct HeadResponseV2 {
+    pub slot: u64,
+    pub block_number: u64,
+    pub block_hash: B256,
+    pub timestamp: u64,
+    pub timestamp_diff: u64,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct ChainHeadResponse {
+    pub head: u32,
+}
+
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub enum StatusEnum {
+pub struct RangeBlocks {
+    pub start: u32,
+    pub end: u32,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct RangeBlocksAPIResponse {
+    pub data: RangeBlocks,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct HeaderBlockNumber {
+    #[serde(deserialize_with = "hex_to_u32")]
+    pub number: u32,
+}
+
+fn hex_to_u32<'de, D>(deserializer: D) -> anyhow::Result<u32, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let s: &str = Deserialize::deserialize(deserializer)?;
+    u32::from_str_radix(s.trim_start_matches("0x"), 16).map_err(serde::de::Error::custom)
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize, sqlx::Type)]
+#[sqlx(type_name = "status")]
+pub enum BridgeStatusEnum {
+    #[sqlx(rename = "initiated")]
+    Initiated,
+    #[sqlx(rename = "in_progress")]
     InProgress,
-    ClaimPending,
+    #[sqlx(rename = "claim_ready")]
+    ClaimReady,
+    #[sqlx(rename = "bridged")]
     Bridged,
 }
-impl ToSql<Status, Pg> for StatusEnum {
-    fn to_sql<'b>(&'b self, out: &mut Output<'b, '_, Pg>) -> serialize::Result {
-        match *self {
-            StatusEnum::InProgress => out.write_all(b"IN_PROGRESS")?,
-            StatusEnum::ClaimPending => out.write_all(b"CLAIM_PENDING")?,
-            StatusEnum::Bridged => out.write_all(b"BRIDGED")?,
-        }
-        Ok(IsNull::No)
-    }
-}
 
-impl FromSql<Status, Pg> for StatusEnum {
-    fn from_sql(bytes: PgValue<'_>) -> deserialize::Result<Self> {
-        match bytes.as_bytes() {
-            b"IN_PROGRESS" => Ok(StatusEnum::InProgress),
-            b"CLAIM_PENDING" => Ok(StatusEnum::ClaimPending),
-            b"BRIDGED" => Ok(StatusEnum::Bridged),
-            _ => Err(format!(
-                "Unrecognized enum variant {}",
-                std::str::from_utf8(bytes.as_bytes()).unwrap()
-            )
-            .as_str()
-            .into()),
-        }
-    }
-}
-
-#[derive(Queryable, Selectable, Insertable, Identifiable, Serialize)]
+#[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
-#[diesel(table_name = crate::schema::avail_sends)]
-#[diesel(primary_key(message_id))]
-#[diesel(check_for_backend(diesel::pg::Pg))]
-#[derive(Clone, Debug)]
-#[serde_as]
-pub struct AvailSend {
-    pub message_id: i64,
-    pub status: StatusEnum,
-    pub source_transaction_hash: String,
-    pub source_block_number: i64,
-    pub source_block_hash: String,
-    pub source_transaction_index: i64,
-    #[serde_as(as = "TimestampSeconds")]
-    pub source_timestamp: NaiveDateTime,
-    pub token_id: String,
-    pub destination_block_number: Option<i64>,
-    pub destination_block_hash: Option<String>,
-    #[serde_as(as = "Option<TimestampSeconds>")]
-    pub destination_timestamp: Option<NaiveDateTime>,
-    pub depositor_address: String,
-    pub receiver_address: String,
-    pub amount: String,
+pub struct TransactionQueryParams {
+    pub eth_address: Option<H160>,
+    pub avail_address: Option<String>,
 }
 
-#[derive(Queryable, Selectable, Insertable, Identifiable, Serialize)]
-#[serde(rename_all = "camelCase")]
-#[diesel(table_name = crate::schema::ethereum_sends)]
-#[diesel(primary_key(message_id))]
-#[diesel(check_for_backend(diesel::pg::Pg))]
-#[derive(Clone, Debug)]
+#[derive(Debug, Serialize, Deserialize, FromRow)]
 #[serde_as]
-pub struct EthereumSend {
-    pub message_id: i64,
-    pub status: StatusEnum,
-    pub source_transaction_hash: String,
-    pub source_block_number: i64,
+#[serde(rename_all = "camelCase")]
+pub struct EthTransactionRow {
+    pub message_id: BigDecimal,
+    pub sender: String,
+    pub receiver: String,
     pub source_block_hash: String,
-    #[serde_as(as = "TimestampSeconds")]
-    pub source_timestamp: NaiveDateTime,
-    pub token_id: String,
-    pub destination_block_number: Option<i64>,
-    pub destination_block_hash: Option<String>,
-    pub destination_transaction_index: Option<i64>,
-    #[serde_as(as = "Option<TimestampSeconds>")]
-    pub destination_timestamp: Option<NaiveDateTime>,
-    pub depositor_address: String,
-    pub receiver_address: String,
+    pub source_transaction_hash: String,
     pub amount: String,
+    pub timestamp: i64,
+    pub final_status: BridgeStatusEnum,
+    pub source_block_height: i32,
+    pub destination_block_height: Option<i32>,
+    pub destination_tx_index: Option<i32>,
+}
+
+#[derive(Debug, Serialize, Deserialize, FromRow)]
+#[serde_as]
+#[serde(rename_all = "camelCase")]
+pub struct AvailTransactionRow {
+    pub message_id: BigDecimal,
+    pub sender: Option<String>,
+    pub receiver: String,
+    pub source_block_hash: String,
+    pub source_transaction_hash: String,
+    pub amount: String,
+    pub block_timestamp: i64,
+    pub final_status: BridgeStatusEnum,
+    pub source_block_height: i32,
+    pub source_tx_index: i32,
+    pub destination_tx_hash: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum TxDirection {
+    AvailEth,
+    EthAvail,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde_as]
+#[serde(rename_all = "camelCase")]
+pub struct TransactionData {
+    pub direction: TxDirection,
+    pub message_id: BigDecimal,
+    pub sender: String,
+    pub receiver: String,
+    pub source_block_hash: String,
+    pub source_tx_hash: String,
+    pub amount: String,
+    pub status: BridgeStatusEnum,
+    pub timestamp: i64,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub claim_estimate: Option<u64>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub source_block_number: Option<i32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub source_tx_index: Option<i32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub destination_block_number: Option<i32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub destination_tx_index: Option<i32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub destination_tx_hash: Option<String>,
+}
+
+impl TransactionData {
+    pub fn new(
+        direction: TxDirection,
+        message_id: BigDecimal,
+        sender: String,
+        receiver: String,
+        source_block_hash: String,
+        source_tx_hash: String,
+        amount: String,
+        status: BridgeStatusEnum,
+        timestamp: i64,
+        claim_estimate: Option<u64>,
+        source_block_number: Option<i32>,
+        source_tx_index: Option<i32>,
+        destination_block_number: Option<i32>,
+        destination_tx_index: Option<i32>,
+        destination_tx_hash: Option<String>,
+    ) -> Self {
+        Self {
+            direction,
+            message_id,
+            sender,
+            receiver,
+            source_block_hash,
+            source_tx_hash,
+            amount,
+            status,
+            timestamp,
+            claim_estimate,
+            source_block_number,
+            source_tx_index,
+            destination_block_number,
+            destination_tx_index,
+            destination_tx_hash,
+        }
+    }
 }
